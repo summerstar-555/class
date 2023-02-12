ee80d4fe006ff36ae6300a4b87955dd26991ea5c
summerstar555 <3577295397@qq.com>
2023-02-12 17:09:28 +0800

    2023年2月12日17:09:25

--- main.py
+++ main.py
@@ -242,3 +242,31 @@ d = D()
 d.add(2)
 print(d.n)
 """
+
+# super例子
+"""
+class FooParent(object):
+    def __init__(self):
+        self.parent = 'I\'m the parent.'
+        print('Parent')
+
+    def bar(self, message):
+        print("%s from Parent" % message)
+
+
+class FooChild(FooParent):
+    def __init__(self):
+        # super(FooChild,self) 首先找到 FooChild 的父类（就是类 FooParent），然后把类 FooChild 的对象转换为类 FooParent 的对象
+        super(FooChild, self).__init__()
+        print('Child')
+
+    def bar(self, message):
+        super(FooChild, self).bar(message)      # python3可省略super内的参数
+        print('Child bar fuction')
+        print(self.parent)
+
+
+if __name__ == '__main__':
+    fooChild = FooChild()
+    fooChild.bar('HelloWorld')
+"""
